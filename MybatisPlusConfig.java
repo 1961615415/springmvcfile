7bf3bec4fe6c5cb0c418a65177c96ae031e5c982
dcx <1961615415@qq.com>
2022-01-06 17:15:14 +0800

    Add files via upload

---
+++ MybatisPlusConfig.java
@@ -0,0 +1,33 @@
+package com.wmbs.conf;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/***
+ * 必须要放到Application的自包里，要不找不到
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.wmbs.dao.mapper")
+public class MybatisPlusConfig {
+
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.ORACLE));
+        return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    }
+}
+
