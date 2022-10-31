e114837754edeef54aafca7b10dc24242731085a
dcx <1961615415@qq.com>
2022-10-31 17:38:11 +0800

    Add files via upload

---
+++ UserPermissionMapper.java
@@ -0,0 +1,16 @@
+package com.wmbs.dao.mapper;
+
+import com.wmbs.dao.entity.UserPermission;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wm
+ * @since 2021-12-27
+ */
+public interface UserPermissionMapper extends BaseMapper<UserPermission> {
+
+}
