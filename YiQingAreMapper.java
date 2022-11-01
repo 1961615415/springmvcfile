80b32ac8abe8ae3694008def75ea87ab65a1cf74
dcx <1961615415@qq.com>
2022-11-01 10:04:01 +0800

    Add files via upload

---
+++ YiQingAreMapper.java
@@ -0,0 +1,16 @@
+package com.wmbs.dao.mapper;
+
+import com.wmbs.dao.entity.YiQingAre;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wm
+ * @since 2021-12-22
+ */
+public interface YiQingAreMapper extends BaseMapper<YiQingAre> {
+
+}
