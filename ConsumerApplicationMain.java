2d6c061ac1761ee0fc19e51d572ad10c17140858
dcx <1961615415@qq.com>
2022-11-03 18:26:36 +0800

    Add files via upload

---
+++ ConsumerApplicationMain.java
@@ -0,0 +1,11 @@
+package com.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplicationMain.class, args);
+    }
+}
