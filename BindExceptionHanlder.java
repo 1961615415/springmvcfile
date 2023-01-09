0c6e7fb29cfaaea2b0794cb854abbd1debb04e32
dcx <1961615415@qq.com>
2023-01-09 13:48:15 +0800

    Add files via upload

--- BindExceptionHanlder.java
+++ BindExceptionHanlder.java
@@ -1,46 +1,47 @@
-package com.wmbs.conf;
+package cn.knet.wz.conf;
 
-import com.wmbs.vo.LayResult;
-import lombok.extern.slf4j.Slf4j;
+import cn.knet.domain.vo.APIResult;
+import lombok.extern.java.Log;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+/**
+ * @author xuxiannian
+ */
 @RestControllerAdvice
-@Slf4j
+@Log
 public class BindExceptionHanlder {
 
 
     @ExceptionHandler(BindException.class)
-    public LayResult<String> handleBindException(BindException ex) {
+    public APIResult handleBindException(BindException ex) {
+        ex.printStackTrace();
+
         FieldError fieldError = ex.getFieldError();
-        log.info("参数校验异常：{}", fieldError.getDefaultMessage());
-        if (fieldError.getDefaultMessage().startsWith("Failed to convert property value of type")) {
-            return LayResult.error(1002, fieldError.getField() + ":参数类型不匹配");
-        }
-        return LayResult.error(1002, fieldError.getDefaultMessage());
+        return APIResult.error(1002, fieldError.getDefaultMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public LayResult<String> handleBindException(IllegalArgumentException ex) {
-        log.info("数据校验异常：{}", ex.getMessage());
+    public APIResult handleBindException(IllegalArgumentException ex) {
+
         ex.printStackTrace();
-        return LayResult.error(1003, ex.getMessage());
+        return APIResult.error(1003, ex.getMessage());
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public LayResult<String> handleBindException(NoHandlerFoundException ex) {
+
+    public APIResult handleBindException(NoHandlerFoundException ex) {
         ex.printStackTrace();
-        log.info("绑定异常：{}", ex.getMessage());
-        return LayResult.error(1004, ex.getMessage());
+
+        return APIResult.error(1004, ex.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public LayResult<String> handleBindException(RuntimeException ex) {
-        log.info("运行时异常：{}", ex.getMessage());
+    public APIResult handleBindException(RuntimeException ex) {
         ex.printStackTrace();
-        return LayResult.error(1005, ex.getMessage());
+        return APIResult.error(1005, ex.getMessage());
     }
 }
